0842141a95646c6f8906c054ff35124b35df3f8f
Jaime Salas Zancada <szjaime@gmail.com>
2017-03-08 02:09:07 +0100

    Started BEM students SASS

--- gulpfile.js
+++ gulpfile.js
@@ -33,19 +33,6 @@ const openOptions = {
   app: resolveBrowser()
 };
 
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-//
-// gulp.task('sass', function () {
-//   return gulp.src('./sass/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./css'));
-// });
-
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./sass/**/*.scss', ['sass']);
-// });
-
 gulp.task('sass',
   () => gulp.src('./src/sass/**/*.scss')
             .pipe(sass().on('error', sass.logError))
